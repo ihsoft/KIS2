4e28c1dc81f057d8a6743d5c6fd5e1ee28927dff
ihsoft <igor.zavoychinskiy@gmail.com>
2019-09-15 23:30:44 -0700

    Add horizontal control

---
+++ Source/KIS.Unity/Scripts/UIKISHorizontalSliderControl.cs
@@ -0,0 +1,40 @@
+﻿// Kerbal Inventory System v2
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using KSPDev.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KIS2 {
+
+/// <summary>KSP alike range value selection control.</summary>
+public sealed class UIKISHorizontalSliderControl : UIControlBaseScript {
+  #region Unity serialized fields
+  [SerializeField]
+  Slider sliderControl = null;
+
+  [SerializeField]
+  Text sliderText = null;
+  #endregion
+
+  #region API properties
+  /// <summary>Text which is shown on top of the scroll bar area.</summary>
+  /// <remarks>Can be set to empty string to hide the control.</remarks>
+  public string text {
+    get { return sliderText.text; }
+    set {
+      sliderText.text = value;
+      sliderText.gameObject.SetActive(!string.IsNullOrEmpty(value));
+    }
+  }
+
+  /// <summary>Current value of the slider.</summary>
+  public float value {
+    get { return sliderControl.value; }
+    set { sliderControl.value = value; }
+  }
+  #endregion
+}
+
+}  // namespace
