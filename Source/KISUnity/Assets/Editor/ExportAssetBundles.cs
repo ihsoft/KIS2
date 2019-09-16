d5dacf42d6e6288ee21d8d4e8e844ea78e62b981
ihsoft <igor.zavoychinskiy@gmail.com>
2019-09-15 21:39:55 -0700

    Unity export bundle script

---
+++ Source/KISUnity/Assets/Editor/ExportAssetBundles.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ExportAssetBundles {
+  [MenuItem("Assets/Build AssetBundle")]
+  static void ExportResource()
+  {
+    // Build for Windows platform.
+    BuildPipeline.BuildAssetBundles(
+        Application.streamingAssetsPath,
+        BuildAssetBundleOptions.None,
+        BuildTarget.StandaloneWindows64);
+
+    // Refresh the Project folder.
+    AssetDatabase.Refresh();
+  }
+}
