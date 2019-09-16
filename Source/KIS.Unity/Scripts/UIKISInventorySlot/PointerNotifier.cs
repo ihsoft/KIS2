80fa25f251a7d3d14435e9856d71b7290ba33079
ihsoft <igor.zavoychinskiy@gmail.com>
2019-09-15 23:02:30 -0700

    Add inventory slot classes

---
+++ Source/KIS.Unity/Scripts/UIKISInventorySlot/PointerNotifier.cs
@@ -0,0 +1,13 @@
+﻿// Kerbal Inventory System
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using KSPDev.Unity;
+
+namespace KIS2.UIKISInventorySlot {
+
+/// <summary>Script, that delivers all slot clicks to the controller.</summary>
+public sealed class PointerNotifier : GenericPointerNotifier<Slot, UIKISInventoryWindow> {
+}
+
+}  // namespace
---
+++ Source/KIS.Unity/Scripts/UIKISInventorySlot/Slot.cs
@@ -0,0 +1,104 @@
+﻿// Kerbal Inventory System v2
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using KSPDev.Unity;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KIS2.UIKISInventorySlot {
+
+/// <summary>Controller for a KIS inventory slot.</summary>
+public sealed class Slot : UIControlBaseScript {
+  #region Unity serialized fields
+  [SerializeField]
+  RawImage partImage = null;
+
+  [SerializeField]
+  Text stackSizeIndicatorText = null;
+
+  [SerializeField]
+  GameObject resourceIndicatorControl = null;
+
+  [SerializeField]
+  Text resourceIndicatorText = null;
+
+  [SerializeField]
+  GameObject scienceIndicatorControl = null;
+
+  [SerializeField]
+  GridLayoutGroup bottomControlsGrid = null;
+  #endregion
+
+  #region API properties and fields
+  /// <summary>Zero based index of the slot.</summary>
+  public int slotIndex {
+    get { return transform.GetSiblingIndex(); }
+  }
+
+  /// <summary>Image to show as the slot's background.</summary>
+  /// <remarks>If set to <c>null</c>, then the background is transparent.</remarks>
+  public Texture slotImage {
+    get { return partImage.gameObject.activeSelf ? partImage.texture : null; }
+    set {
+      partImage.texture = value;
+      partImage.gameObject.SetActive(value != null);
+    }
+  }
+
+  /// <summary>String that identifies the slot's overall resources state</summary>
+  /// <remarks>Only shown if not empty.</remarks>
+  public string resourceStatus {
+    get { return resourceIndicatorText.text; }
+    set {
+      resourceIndicatorText.text = value;
+      resourceIndicatorControl.gameObject.SetActive(!string.IsNullOrEmpty(value));
+    }
+  }
+
+  /// <summary>Size of the stack in the slot.</summary>
+  /// <remarks>Only shown if not empty.</remarks>
+  public string stackSize {
+    get { return stackSizeIndicatorText.text; }
+    set {
+      stackSizeIndicatorText.text = value;
+      stackSizeIndicatorText.gameObject.SetActive(!string.IsNullOrEmpty(value));
+    }
+  }
+
+  /// <summary>Tells if part in the slot has science.</summary>
+  public bool hasScience {
+    get { return scienceIndicatorControl.gameObject.activeSelf; }
+    set { scienceIndicatorControl.gameObject.SetActive(value); }
+  }
+  #endregion
+
+  #region API methods
+  /// <summary>Clears all UI controls, making the slot to look completely empty.</summary>
+  public void ClearContent() {
+    partImage.texture = null;
+    resourceStatus = null;
+    stackSize = null;
+    slotImage = null;
+    hasScience = false;
+    while (bottomControlsGrid.transform.childCount > 0) {
+      DestroyImmediate(bottomControlsGrid.transform.GetChild(0).gameObject);
+    }
+  }
+
+  /// <summary>Sets a prefab state.</summary>
+  /// <remarks>
+  /// This will be a base state to create a new slot in the game. It only makes sense to call this
+  /// method during the prefab initialization phase.
+  /// </remarks>
+  /// <seealso cref="UIPrefabBaseScript"/>
+  public void InitPrefab() {
+    //FIXME: Capture the first button as prefab.  
+    ClearContent();
+    gameObject.SetActive(false);
+  }
+  #endregion
+}
+
+}  // namespace
