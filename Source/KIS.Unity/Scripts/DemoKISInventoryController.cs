0f93a89ba6588b0f03bec2f0bf1cdd696b251ac6
ihsoft <igor.zavoychinskiy@gmail.com>
2019-09-17 00:09:29 -0700

    Add demo controller for Unity editor

---
+++ Source/KIS.Unity/Scripts/DemoKISInventoryController.cs
@@ -0,0 +1,69 @@
+﻿// Kerbal Inventory System v2
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System;
+using KIS2.UIKISInventorySlot;
+using KSPDev.Unity;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace KIS2 {
+
+/// <summary>Helper class to debug behavior in Unity.</summary>
+/// <remarks>
+/// This class must be removed before making the final prefabs. However, even if it manages to
+/// sneak into the game, it will simply not start there.
+/// </remarks>
+public sealed class DemoKISInventoryController : UIControlBaseScript {
+
+  #region For Unity customization
+  public int limitGridHeight = 1;
+  public int limitGridWidth = 3;
+  public string hintTitle = "test slot";
+  #endregion
+
+  #region MonoBehaviour overrides
+  void Start() {
+    if (Application.isEditor) {
+      var kisWindow = GetComponent<UIKISInventoryWindow>();
+      kisWindow.onSlotHover.Add(OnSlotHover);
+      kisWindow.onSlotClick.Add(OnSlotClick);
+      kisWindow.onSlotAction.Add(OnSlotAction);
+      kisWindow.onGridSizeChange.Add(OnSizeChanged);
+    } else {
+      enabled = false;
+    }
+  }
+  #endregion
+
+  #region Local utlity methods
+  void OnSlotHover(UIKISInventoryWindow host, Slot slot, bool isHover) {
+    LogInfo("Pointer hover: slot={0}, isHover={1}", slot.slotIndex, isHover);
+    if (isHover) {
+      var tooltip = host.StartSlotTooltip();
+      tooltip.title = hintTitle;
+      tooltip.UpdateLayout();
+    }
+  }
+
+  void OnSlotClick(
+      UIKISInventoryWindow host, Slot slot, PointerEventData.InputButton button) {
+    LogInfo("Clicked: slot={0}, button={1}", slot.slotIndex, button);
+  }
+
+  void OnSlotAction(
+      UIKISInventoryWindow host, Slot slot, int actionButtonNum,
+      PointerEventData.InputButton button) {
+    LogInfo("Clicked: slot={0}, action={1}, button={2}", slot.slotIndex, actionButtonNum, button);
+  }
+
+  Vector2 OnSizeChanged(UIKISInventoryWindow host, Vector2 oldSize, Vector2 newSize) {
+    return new Vector2(
+        newSize.x <= limitGridWidth ? newSize.x : limitGridWidth,
+        newSize.y <= limitGridHeight ? newSize.y : limitGridHeight);
+  }
+  #endregion
+}
+
+}  // namespace
