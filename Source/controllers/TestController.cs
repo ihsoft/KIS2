89ec036a58b5eeec723e4b5548f42ec1e59d0e2f
ihsoft <igor.zavoychinskiy@gmail.com>
2019-09-18 23:59:33 -0700

    Add debug staff

---
+++ Source/controllers/TestController.cs
@@ -0,0 +1,77 @@
+﻿// Kerbal Inventory System
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using KSPDev.LogUtils;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using KSP.UI;
+using System.Reflection;
+using System;
+using KSPDev.ModelUtils;
+
+namespace KIS2 {
+
+[KSPAddon(KSPAddon.Startup.FlightAndEditor, false /*once*/)]
+public sealed class TestController : MonoBehaviour {
+
+  public string openGUIKey = "Tab";
+  static Event openGUIEvent;
+
+  void Awake() {
+    if (!string.IsNullOrEmpty(openGUIKey)) {
+      openGUIEvent = Event.KeyboardEvent(openGUIKey);
+    }
+  }
+
+  UIKISInventoryWindowBase inventoryWindow;
+
+  void DumpDlg(Transform dlg) {
+    foreach (var path in Hierarchy.ListHirerahcy(dlg)) {
+      var obj = Hierarchy.FindTransformByPath(dlg.parent, path);
+      DebugEx.Warning("*** for path: {0}", path);
+      if (obj != null) {
+        foreach (var c in obj.GetComponents<UnityEngine.Object>()) {
+          DebugEx.Warning("type={0}", c.GetType());
+        }
+      }
+    }
+  }
+  
+  void OnGUI() {
+    if (openGUIEvent != null && Event.current.Equals(openGUIEvent)) {
+      Event.current.Use();
+      if (inventoryWindow != null) {
+        inventoryWindow.CloseWindow();
+      } else {
+        inventoryWindow = UIKISInventoryWindowBase.CreateDialog(
+            "KISInventoryDialog", "Inventory: FOO");
+      }
+    }
+    
+    if (Event.current.Equals(Event.KeyboardEvent("L")) && inventoryWindow != null) {
+      inventoryWindow.dlgTitle += " L"; 
+    }
+    if (Event.current.Equals(Event.KeyboardEvent("1")) && inventoryWindow != null) {
+      inventoryWindow.SetGridSize(3, 1);
+    }
+    if (Event.current.Equals(Event.KeyboardEvent("2")) && inventoryWindow != null) {
+      inventoryWindow.SetGridSize(3, 2);
+    }
+    if (Event.current.Equals(Event.KeyboardEvent("3")) && inventoryWindow != null) {
+      inventoryWindow.SetGridSize(3, 3);
+    }
+    if (Event.current.Equals(Event.KeyboardEvent("4")) && inventoryWindow != null) {
+      inventoryWindow.SetGridSize(4, 1);
+    }
+    if (Event.current.Equals(Event.KeyboardEvent("5")) && inventoryWindow != null) {
+      inventoryWindow.SetGridSize(4, 2);
+    }
+    if (Event.current.Equals(Event.KeyboardEvent("6")) && inventoryWindow != null) {
+      inventoryWindow.SetGridSize(4, 3);
+    }
+  }
+}
+
+}
---
+++ Source/controllers/UIKISInventoryWindowBase.cs
@@ -0,0 +1,200 @@
+﻿// Kerbal Inventory System
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using KSP.UI;
+using System;
+using System.Linq;
+using UnityEngine;
+using KSPDev.LogUtils;
+using KSPDev.ModelUtils;
+using KSPDev.Prefabs;
+using KSPDev.Unity;
+using KIS2.UIKISInventorySlot;
+
+namespace KIS2 {
+  
+public sealed class UIKISInventoryWindowBase : UIScalableWindowController {
+
+  public string dlgTitle {
+    get { return unityWindow.title; }
+    set { unityWindow.title = value; }
+  }
+
+  public int minGridWidth = 3;
+  public int minGridHeight = 1;
+
+  UIKISInventoryWindow unityWindow;
+
+  #region UIScalableWindowController overrides
+  public override void Awake() {
+    base.Awake();
+    unityWindow = GetComponent<UIKISInventoryWindow>();
+  }
+  #endregion
+
+  #region Factory methods
+  //FIXME: make it protected and expose from the descendatnts
+  public static UIKISInventoryWindowBase CreateDialog(string name, string title) {
+    DebugEx.Fine("Create KIS inventory window: {0}", title);
+    var dlg = UnityPrefabController.CreateInstance<UIKISInventoryWindowBase>(
+        name, UIMasterController.Instance.actionCanvas.transform);
+    dlg.dlgTitle = title;
+    return dlg;
+  }
+  #endregion
+
+  #region API methods
+  public void CloseWindow() {
+    DebugEx.Fine("Destroy KIS inventory window: {0}", dlgTitle);
+    Hierarchy.SafeDestory(gameObject);
+  }
+
+  public bool SetGridSize(int width, int height) {
+    //FIXME
+    return false;
+  }
+
+//  public bool SetGridSize(int x, int y) {
+//    //FIXME: for the demo!
+//    for (var i = slots.Count - 1; i >= 0; --i) {
+//      UnityEngine.Object.DestroyImmediate(slots[i].gameObject);
+//    }
+//    slots.Clear();
+//    
+//    if (x < minGridWidth || y < minGridHeight) {
+//      DebugEx.Warning(
+//          "Cannot set size lower than minimum: x={0}, y={1}", minGridWidth, minGridHeight);
+//      return false;
+//    }
+//    var newSize = x * y;
+//    //var usedSize = slots.Count(s => s.hasContent);
+//    var usedSize = 0;
+//    //FIXME
+//    DebugEx.Warning("*** sizes: old={0}, new={1}, used={2}", slots.Count, newSize, usedSize);
+//    if (newSize < usedSize) {
+//      return false;
+//    }
+//    unityWindow.slotsGrid.constraintCount = x;
+//    if (newSize > slots.Count) {
+//      for (var i = newSize - slots.Count; i > 0; --i) {
+//        AddEmptySlot();
+//      }
+//    } else if (newSize < slots.Count) {
+//      //var unusedSlots = slots.Where(s => !s.hasContent).ToArray();
+//      var unusedSlots = slots.ToArray();
+//      //FIXME
+//      DebugEx.Warning("*** found unused slots: {0}", unusedSlots.Length);
+//      var targetSlot = 0;
+//      for (var i = slots.Count - newSize; i > 0 && targetSlot < unusedSlots.Length; --i) {
+//        var slot = unusedSlots[targetSlot++];
+//        slots.Remove(slot);
+//        UnityEngine.Object.DestroyImmediate(slot.gameObject);
+//      }
+//    }
+//    
+//    return false;
+//  }
+  
+  //FIXME: make it private
+//  public bool AddEmptySlot() {
+//    var window = GetComponent<UIKISInventoryWindow>();
+//    var slot = window.AddSlot();
+//    slots.Add(slot);
+//    
+//    const string samplePartName = "KIS.Container1";
+//    
+//    var names = new string[] {
+//      "KIS.bomb1",
+//      "kis.concreteBase1",
+//      "KIS.Container1",
+//      "KIS.Container2",
+//      "KIS.Container3",
+//      "KIS.Container4",
+//      "KIS.Container5",
+//      "KIS.Container6",
+//      "KIS.Container7",
+//      "KIS.Container8",
+//      "KIS.ContainerMount1",
+//      "KIS.electricScrewdriver",
+//      "KIS.evapropellant",
+//      "KIS.guide",
+//      "KIS.wrench",
+//    };
+//    var avPartIndex = (int) (UnityEngine.Random.value * names.Length);
+//    var avPart = PartLoader.getPartInfoByName(names[avPartIndex]);
+//    if (avPart == null) {
+//      DebugEx.Error("*** bummer: no part {0}", samplePartName);
+//    }
+//    var icon = KISAPI.PartIconUtils.MakeDefaultIcon(avPart, 256, null);
+//    slot.SetContent(icon, OnSlotClicked, OnSlotActionClicked);
+//    
+//    if (UnityEngine.Random.value < 0.20f) {
+//      slot.hasScience = true;
+//    }
+//    if (UnityEngine.Random.value < 0.3f) {
+//      SetResourceAmount(UnityEngine.Random.value, slot);
+//    }
+//    
+//    //AsyncCall.CallOnEndOfFrame(this, ControlMoved);
+//    return true;
+//  }
+
+  void SetResourceAmount(double percent, Slot slot) {
+    string text;
+    if (percent < 0.05) {
+      text = "<5%";
+    } else if (percent < 0.15) {
+      text = "~10%";
+    } else if (percent < 0.25) {
+      text = "~20%";
+    } else if (percent < 0.35) {
+      text = "~30%";
+    } else if (percent < 0.45) {
+      text = "~40%";
+    } else if (percent < 0.55) {
+      text = "~50%";
+    } else if (percent < 0.65) {
+      text = "~60%";
+    } else if (percent < 0.75) {
+      text = "~70%";
+    } else if (percent < 0.85) {
+      text = "~80%";
+    } else if (percent < 0.95) {
+      text = "~90%";
+    } else if (percent - 1 > double.Epsilon) {
+      text = ">95%";
+    } else {
+      text = null;
+    }
+    slot.resourceStatus = text;
+  }
+  #endregion
+
+  #region Internal methods
+  /// <summary>
+  /// Called from <see cref="KISModLoadController"/> when it's time to initialize.
+  /// </summary>
+  internal static void OnGameLoad() {
+    PrefabLoader.LoadAllAssets(KSPUtil.ApplicationRootPath + "GameData/KIS/Prefabs/ui_prefabs");
+    //FIXME register as base class
+    var dialog = UnityPrefabController.GetPrefab<UIKISInventoryWindow>();
+    var baseType = dialog.gameObject.AddComponent<UIKISInventoryWindowBase>();
+    UnityPrefabController.RegisterPrefab(baseType, baseType.name);
+  }
+  #endregion
+  
+  #region Debug methods - DROP
+  public static void DumpElement(Transform el) {
+    DebugEx.Warning("*** For element: {0}", DbgFormatter.TranformPath(el));
+    foreach (var component in el.gameObject.GetComponents<Component>()) {
+      DebugEx.Warning("Component: {0}", component.GetType());
+    }
+    for (var i = 0; i < el.childCount; ++i) {
+      DumpElement(el.GetChild(i));
+    }
+  }
+  #endregion
+}
+
+}  // namespace
