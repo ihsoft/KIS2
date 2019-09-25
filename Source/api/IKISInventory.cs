d2b09aeac73b7ad68ea5643f70676709d9aa78f6
ihsoft <igor.zavoychinskiy@gmail.com>
2019-09-24 23:06:43 -0700

    Improve main inventory interface and add base for it
    - Reflect mass/cost/vlume props.
    - Introduce size prop.
    - Introduce
    update stats method.
    - Simplify AddItem().

--- Source/api/IKISInventory.cs
+++ Source/api/IKISInventory.cs
@@ -3,6 +3,7 @@
 // License: Public Domain
 
 using System;
+using UnityEngine;
 
 namespace KISAPIv2 {
 
@@ -11,22 +12,44 @@ public interface IKISInventory {
   /// <summary>Items in the inventory.</summary>
   InventoryItem[] items { get; }
 
-  /// <summary>Adds a new item into the inventory.</summary>
+  /// <summary>Maximum dimensions of the inner inventory space.</summary>
+  /// <remarks>Objects that are large in any of the dimensions cannot fit the inventory.</remarks>
+  Vector3 maxInnerSize { get; }
+
+  /// <summary>Maximum volume of all the items that inventory can hold.</summary>
+  /// <seealso cref="usedVolume"/>
+  double maxVolume { get; }
+
+  /// <summary>Current volume of all the items in the inventory.</summary>
+  /// <seealso cref="maxVolume"/>
+  /// <seealso cref="UpdateInventoryStats"/>
+  double usedVolume { get; }
+
+  /// <summary>Total mass of all the items in the inventory.</summary>
+  /// <seealso cref="UpdateInventoryStats"/>
+  double contentMass { get; }
+
+  /// <summary>Total cost of all the items in the inventory.</summary>
+  /// <seealso cref="UpdateInventoryStats"/>
+  double contentCost { get; }
+
+  /// <summary>Verifies if the item can be added into the inventory.</summary>
   /// <param name="avPart">The part proto.</param>
   /// <param name="node">The part's persisted state.</param>
-  /// <param name="variant">
-  /// The part's variant. If it's <c>null</c>, then it will be seacrhed in the persisted state.
-  /// </param>
-  /// <returns>The newly created item.</returns>
-  InventoryItem AddItem(AvailablePart avPart, ConfigNode node, PartVariant variant = null);
+  /// <returns><c>null</c> if item can be added, or a list of reasons why not.</returns>
+  /// <seealso cref="AddItem"/>
+  ErrorReason[] CheckCanAdd(AvailablePart avPart, ConfigNode node);
 
-  /// <summary>Adds a new item into the inventory from a real part in the scene.</summary>
+  /// <summary>Adds a new item into the inventory.</summary>
   /// <remarks>
-  /// The original part won't be affected. Only its copy will be saved into inventory.
+  /// This method does <i>not</i> verifies if the item can fit the inventory. Doing this check is
+  /// responsibility of the caller.
   /// </remarks>
-  /// <param name="part">The real part in the secene to capture.</param>
-  /// <returns>The newly created item.</returns>
-  InventoryItem AddItem(Part part);
+  /// <param name="avPart">The part proto.</param>
+  /// <param name="node">The part's persisted state.</param>
+  /// <returns>The newly created item or <c>null</c> if add failed.</returns>
+  /// <seealso cref="CheckCanAdd"/>
+  InventoryItem AddItem(AvailablePart avPart, ConfigNode node);
 
   /// <summary>Removes the specified item from inventory.</summary>
   /// <remarks>Locked items cannot be removed.</remarks>
@@ -43,6 +66,18 @@ public interface IKISInventory {
   /// <seealso cref="DeleteItem"/>
   /// <seealso cref="InventoryItem.isLocked"/>
   void SetItemLock(InventoryItem item, bool isLocked);
+
+  /// <summary>Forces the container to recalculate its mass/cost/volume stats.</summary>
+  /// <remarks>
+  /// Before updating own state, the inventory will update every single item in it. An exception
+  /// will be made if <paramref name="changedItems"/> are provided. In this case only that items
+  /// will be updated. Callers can use this ability to optimize their calls and save CPU. 
+  /// </remarks>
+  /// <param name="changedItems">
+  /// The items, which changed state was the reason of the inventory update. If empty, then all the
+  /// items in the inventory will be updated.
+  /// </param>
+  void UpdateInventoryStats(params InventoryItem[] changedItems);
 }
 
 }  // namespace
---
+++ Source/modules/KISContainerBase.cs
@@ -0,0 +1,127 @@
+﻿// Kerbal Inventory System
+// Module author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using KSPDev.LogUtils;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using KSP.UI;
+using System.Reflection;
+using System;
+using System.Linq;
+using KSPDev.GUIUtils;
+using KSPDev.ModelUtils;
+using KSPDev.PartUtils;
+using KSPDev.KSPInterfaces;
+using KSPDev.GUIUtils.TypeFormatters;
+using KISAPIv2;
+using KIS2.UIKISInventorySlot;
+using KIS2.GUIUtils;
+
+namespace KIS2 {
+
+  //FIXME: separate container and inventory concepts. data vs UI
+public class KISContainerBase : AbstractPartModule,
+    IKISInventory {
+
+  #region Part's config fields
+  /// <summary>Maximum volume that this container can contain.</summary>
+  /// <include file="SpecialDocTags.xml" path="Tags/ConfigSetting/*"/>
+  [KSPField]
+  public double maxContainerVolume;
+
+  /// <summary>Maximum size of the item that can fit the contianer.</summary>
+  /// <include file="SpecialDocTags.xml" path="Tags/ConfigSetting/*"/>
+  [KSPField]
+  public Vector3 maxItemSize;
+  #endregion
+
+  #region IKISInventory properties
+  /// <inheritdoc/>
+  public InventoryItem[] items {
+    get { return itemsList.ToArray(); }
+  }
+  readonly List<InventoryItem> itemsList = new List<InventoryItem>();
+
+  /// <inheritdoc/>
+  public Vector3 maxInnerSize { get { return maxItemSize; } }
+
+  /// <inheritdoc/>
+  public double maxVolume { get { return maxContainerVolume; } }
+
+  /// <inheritdoc/>
+  public double usedVolume {
+    get {
+      if (_usedVolume < 0) {
+        _usedVolume = itemsList.Sum(i => i.volume);
+      }
+      return _usedVolume;
+    }
+  }
+  double _usedVolume = -1;
+
+  /// <inheritdoc/>
+  public double contentMass {
+    get {
+      if (_contentMass < 0) {
+        _contentMass = itemsList.Sum(i => i.fullMass);
+      }
+      return _contentMass;
+    }
+  }
+  double _contentMass = -1;
+
+  public double contentCost {
+    get {
+      if (_contentCost < 0) {
+        _contentCost = itemsList.Sum(i => i.fullCost);
+      }
+      return _contentCost;
+    }
+  }
+  double _contentCost;
+  #endregion
+
+  #region IKISInventory implementation
+  /// <inheritdoc/>
+  public virtual ErrorReason[] CheckCanAdd(AvailablePart avPart, ConfigNode node) {
+    //FIXME: verify part's volume and size
+    return null;
+  }
+
+  /// <inheritdoc/>
+  public virtual InventoryItem AddItem(AvailablePart avPart, ConfigNode node) {
+    var item = new InventoryItemImpl(this, avPart, node);
+    itemsList.Add(item);
+    UpdateInventoryStats(item);
+    return item;
+  }
+
+  /// <inheritdoc/>
+  public virtual ErrorReason[] DeleteItem(InventoryItem item) {
+    throw new NotImplementedException();
+  }
+
+  /// <inheritdoc/>
+  public virtual void SetItemLock(InventoryItem item, bool isLocked) {
+    throw new NotImplementedException();
+  }
+
+  /// <inheritdoc/>
+  //public virtual void UpdateInventoryStats() {
+  public virtual void UpdateInventoryStats(params InventoryItem[] changedItems) {
+    if (changedItems.Length > 0) {
+      changedItems.ToList().ForEach(i => i.UpdateConfig());
+    } else {
+      DebugEx.Fine("Updating all items in the inventory...");
+      itemsList.ForEach(i => i.UpdateConfig());
+    }
+    _usedVolume = -1;
+    _contentMass = -1;
+    _contentCost = -1;
+  }
+  #endregion
+}
+
+}  // namespace
