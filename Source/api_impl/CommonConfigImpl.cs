1939f90d111c380e921486726d7ccc4bc8d5767d
ihsoft <igor.zavoychinskiy@gmail.com>
2020-03-02 01:03:31 -0800

    Add builder mode settings

--- Source/api_impl/CommonConfigImpl.cs
+++ Source/api_impl/CommonConfigImpl.cs
@@ -15,10 +15,18 @@ public sealed class CommonConfigImpl {
   /// <remarks>Use this sound each time the user action cannot be performed.</remarks>
   public string sndPathBipWrong => _sndPathBipWrong;
 
+  /// <summary>Tells if items in the inventories can be freely adjusted in flight.</summary>
+  public bool builderModeEnabled => _builderModeEnabled;
+
   // ReSharper disable once FieldCanBeMadeReadOnly.Local
   // ReSharper disable once ConvertToConstant.Local
   [PersistentField("Sounds/bipWrong")]
   string _sndPathBipWrong = "";
+
+  // ReSharper disable once FieldCanBeMadeReadOnly.Local
+  // ReSharper disable once ConvertToConstant.Local
+  [PersistentField("BuilderMode/enabled")]
+  bool _builderModeEnabled = false;
 
   internal CommonConfigImpl() {
     ConfigAccessor.ReadFieldsInType(GetType(), this);
