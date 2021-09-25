296c074d595a4a1a5a9556dddd1dda166d74c5e4
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2021-09-24 22:04:13 -0700

    Refactor events state machine
    BREAKING change
    - Introduce NULL state.
    - Set the state via a property.
    - Add after state changed callback.

--- Source/impl/EventsHandlerStateMachine.cs
+++ Source/impl/EventsHandlerStateMachine.cs
@@ -9,33 +9,69 @@ using KSPDev.LogUtils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using KSPDev.ProcessingUtils;
 using UnityEngine;
 
 // ReSharper disable once CheckNamespace
 namespace KIS2 {
 
 /// <summary>
-/// Simple state machine for GUI actions that wraps input events and handle them depending on the
-/// state.   
+/// Simple state machine for GUI actions that wraps input events and handle them depending on the state.   
 /// </summary>
 /// <remarks>
-/// It may be used when GUI actions depend on a definite set of states. Instead of coding a lot of
-/// if/then or switch statements, with this class it's possible to define the behavior once and
-/// simply react on the input actions from the callbacks. As a convenience, this class also offers a
-/// way to quickly build a list of actions available in the current mode to show them as a hint in
-/// GUI. 
+/// It may be used when GUI actions depend on a definite set of states. Instead of coding a lot of if/then or switch
+/// statements, with this class it's possible to define the behavior once and simply react on the input actions from the
+/// callbacks. As a convenience, this class also offers a way to quickly build a list of actions available in the
+/// current mode to show them as a hint in GUI. 
 /// </remarks>
 /// <typeparam name="T">The enum to use as the states set.</typeparam>
 public sealed class EventsHandlerStateMachine<T> where T : struct {
+  #region API properties
+
   /// <summary>Current state of the machine.</summary>
-  /// <value>The state that was set explicitly or the default value of the enum.</value>
-  public T currentState { get; private set; }
+  /// <remarks>
+  /// Defines the actions that are currently active. The <c>null</c> state is a special case: it can be set, but it
+  /// cannot have any actions associated with it. So, setting the state to <c>null</c> guarantees there will be no
+  /// actions or hints processed while the machine is in the "null state". 
+  /// </remarks>
+  /// <value>The state or <c>null</c>.</value>
+  /// <seealso cref="HandleActions"/>
+  /// <seealso cref="GetHints"/>
+  public T? currentState {
+    get => _currentState;
+    set {
+      if (!value.HasValue || !_stateHandlers.TryGetValue(value.Value, out _currentStateHandlers)) {
+        _currentStateHandlers = new List<HandlerDef>();
+      }
+      var oldState = _currentState;
+      _currentState = value;
+      if (!oldState.Equals(_currentState)) {
+        SafeCallbacks.Action(() => ONAfterTransition?.Invoke(oldState, _currentState));
+      }
+    }
+  }
+  T? _currentState;
+
+  /// <summary>Delegate to track an arbitrary state transition.</summary>
+  /// <param name="fromState">The state before the change.</param>
+  /// <param name="toState">The state after the change.</param>
+  /// <seealso cref="currentState"/>
+  /// <seealso cref="EventsHandlerStateMachine{T}.ONAfterTransition"/>
+  public delegate void OnStateChangeHandler(T? fromState, T? toState);
+
+  /// <summary>Event that fires when the state machine has changed its state.</summary>
+  /// <remarks>
+  /// The event is fired <i>after</i> the new state has been applied to the state machine and all the transition
+  /// callbacks are handled. It's only fired when the state has actually changed. 
+  /// </remarks>
+  /// <seealso cref="OnStateChangeHandler"/>
+  public event OnStateChangeHandler ONAfterTransition;
+  #endregion
 
   #region Local data structures
   /// <summary>Container for the handler definition.</summary>
   /// <remarks>
-  /// Keep it <c>struct</c> for the performance reasons. This element will be accessed from the
-  /// <c>Update</c> method.
+  /// Keep it <c>struct</c> for the performance reasons. This element will be accessed from the <c>Update</c> method.
   /// </remarks>
   struct HandlerDef {
     /// <summary>Action that triggers the handler.</summary>
@@ -51,41 +87,39 @@ public sealed class EventsHandlerStateMachine<T> where T : struct {
     /// <seealso cref="HandleActions"/>
     public Action actionFn;
 
-    /// <summary>
-    /// Runtime check function that tells if the action should be available at the particular
-    /// moment.
-    /// </summary>
+    /// <summary>Runtime check function that tells if the action should be available at the particular moment.</summary>
     /// <remarks>
-    /// This function is only called during the state change call. If availability changes withing
-    /// the same state, the actor code must refresh the state of the machine. 
+    /// This function is only called during the state change call. If availability changes withing the same state, the
+    /// actor code must refresh the state of the machine. 
     /// </remarks>
-    /// <seealso cref="SetState"/>
+    /// <seealso cref="currentState"/>
     public Func<bool> checkFn;
   }
   #endregion
 
   #region Local fields and properties
   /// <summary>Cached handlers for the current state.</summary>
-  /// <remarks>It's refreshed each time the <see cref="SetState"/> method is called.</remarks>
-  HandlerDef[] _currentStateHandlers = new HandlerDef[0];
+  /// <remarks>It's refreshed each time the <see cref="currentState"/> is called.</remarks>
+  List<HandlerDef> _currentStateHandlers = new();
 
   /// <summary>Handlers for the states.</summary>
-  /// <remarks>It may ont define all the states, defined by <see cref="T"/>.</remarks>
-  readonly Dictionary<T, List<HandlerDef>> _stateHandlers = new Dictionary<T, List<HandlerDef>>();
+  /// <remarks>It may not define all the states declared by <see cref="T"/>.</remarks>
+  readonly Dictionary<T, List<HandlerDef>> _stateHandlers = new();
   #endregion
 
   #region API methods
-  /// <summary>Defines an action for a state.</summary>
+  /// <summary>Defines an action for the state.</summary>
   /// <param name="state">The state to apply the action to.</param>
   /// <param name="hintText">
-  /// The event hint message. It must refer the <paramref name="actionEvent"/>.
+  /// The event hint message. It must refer the <paramref name="actionEvent"/>. If it's <c>null</c> or empty, then it
+  /// won't show up in the <see cref="GetHints"/> result.
   /// </param>
   /// <param name="actionEvent">The <see cref="Event"/> that triggers the action.</param>
   /// <param name="actionFn">The callback to call when the action is triggered.</param>
   /// <param name="checkIfAvailable">
-  /// The callback that tells if the action is available in the current game state. This function is
-  /// only called during the state change call. If availability changed within the same state, then
-  /// a state refresh needs to be triggered via <see cref="SetState"/>.
+  /// The callback that tells if the action is available in the current game state. This function called in every
+  /// <see cref="HandleActions"/> call, so it has to be performance optimized. If not set, then the action is assumed to
+  /// be always active.
   /// </param>
   public void DefineAction(
       T state, Message<KeyboardEventType> hintText, Event actionEvent, Action actionFn,
@@ -102,66 +136,42 @@ public sealed class EventsHandlerStateMachine<T> where T : struct {
     });
   }
 
-  /// <summary>
-  /// Returns a user friendly string that describes all the available events in the current state.
-  /// </summary>
+  /// <summary>Returns a user friendly string that describes all the available events in the current state.</summary>
+  /// <remarks>If the action doesn't have a hint string, then it will be ignored.</remarks>
   /// <param name="separator">The string to use to join hints for multiple events.</param>
   /// <returns>The event hints, joined with the separator.</returns>
   public string GetHints(string separator = "\n") {
-    if (_currentStateHandlers == null) {
-      return "";
-    }
     return string.Join(
         separator,
         _currentStateHandlers
             .Select(x => x.actionHint)
+            .Where(x => !string.IsNullOrEmpty(x))
             .ToList());
   }
 
   /// <summary>
-  /// Checks if any of the events, defined for the current state, is triggered, and calls the
-  /// relevant callback. 
+  /// Checks if any of the events, defined for the current state, is triggered, and calls the relevant callback. 
   /// </summary>
   /// <remarks>
-  /// <para>
-  /// The events are checked in the order of definition. The first matched event is triggered and
-  /// the rest are skipped.
-  /// </para>
-  /// <para>
-  /// This method is optimized for performance. It's intended to be called from methods like
-  /// <c>Update</c> or <c>OnGUI</c>.
-  /// </para>
+  /// <p>
+  /// The events are checked in the order of definition. The first matched event is triggered and the rest are skipped.
+  /// </p>
+  /// <p>This method is optimized for performance. It's intended to be called from the high FPS methods.</p>
   /// </remarks>
   /// <returns><c>true</c> if an action was triggered and the callback was invoked.</returns>
   public bool HandleActions() {
     // Don't use Linq or foreach! This method is called in every frame, so every bit counts.
-    var handlersNum = _currentStateHandlers.Length;
+    var handlersNum = _currentStateHandlers.Count;
     for (var i = 0; i < handlersNum; i++) {
       var handler = _currentStateHandlers[i];
-      if (EventChecker.CheckClickEvent(handler.actionEvent)) {
-        DebugEx.Fine("Triggered action: {0}", KeyboardEventType.Format(handler.actionEvent));
-        handler.actionFn();
-        return true;
+      if ((handler.checkFn != null && !handler.checkFn()) || !EventChecker.CheckClickEvent(handler.actionEvent)) {
+        continue;
       }
+      DebugEx.Fine("Trigger action: event={0}, index={1}", KeyboardEventType.Format(handler.actionEvent), i);
+      handler.actionFn();
+      return true;
     }
     return false;
-  }
-
-  /// <summary>Changes the current state.</summary>
-  /// <remarks>
-  /// The internal state of the machine is cached for the performance reasons. To refresh it,
-  /// simply set the same state again.
-  /// </remarks>
-  /// <param name="newState"></param>
-  public void SetState(T newState) {
-    if (_stateHandlers.TryGetValue(newState, out var stateHandlers)) {
-      _currentStateHandlers = stateHandlers
-          .Where(x => x.checkFn == null || x.checkFn())
-          .ToArray();
-    } else {
-      _currentStateHandlers = new HandlerDef[0];
-    }
-    currentState = newState;
   }
   #endregion
 }
